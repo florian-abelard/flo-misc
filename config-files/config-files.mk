4057feb651f06a9a9cc191f1524be468a34659a7
Florian Abélard <f.abelard@gmail.com>
2020-09-02 16:01:31 +0200

    feat: add config-system-install command

--- config-files/config-files.mk
+++ config-files/config-files.mk
@@ -6,9 +6,9 @@ HOST_CONFIG_SOURCE_PATH := ${HOST_SOURCE_PATH}/config-files
 
 #------------------------------------------------------------------------------
 
-config-install: config-bash-install config-terminator-install config-xfce-install ##@config-files install all system config files
+config-install: config-bash-install config-system-install config-terminator-install ##@config-files install all system configuration
 
-config-bash-install: ##@config-files install bash config files
+config-bash-install: ##@config-files install bash configuration
 	cp ~/.bashrc{,.back}
 	test ! -e ~/.bash_aliases || cp ~/.bash_aliases{,.back}
 	test ! -e ~/.bash_completion || cp ~/.bash_completion{,.back}
@@ -18,16 +18,19 @@ config-bash-install: ##@config-files install bash config files
 	cp ${HOST_CONFIG_SOURCE_PATH}/bash/bash_extra ~/.bash_extra
 	cp ${HOST_CONFIG_SOURCE_PATH}/bash/bash_functions ~/.bash_functions
 
-config-git-install: #@config-files install git config files
+config-system-install: ##@config-files install system configuration
+	echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
+
+config-git-install: #@config-files install git configuration
 	test ! -e ~/.gitconfig || cp ~/.gitconfig{,.back}
 	cp ${HOST_CONFIG_SOURCE_PATH}/git/gitconfig ~/.gitconfig
 
-config-terminator-install: ##@config-files install terminator config files
+config-terminator-install: ##@config-files install terminator configuration
 	test ! -e ~/.config/terminator/config || cp /.config/terminator/config{,.back}
 	test -e ~/.config/terminator/config || mkdir -p ~/.config/terminator/
 	cp ${HOST_CONFIG_SOURCE_PATH}/terminator/config ~/.config/terminator/
 
-config-xfce-install: ##@config-files install xfce config files
+config-xfce-install: ##@config-files install xfce configuration
 	cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml{,.back}
 	cp ${HOST_CONFIG_SOURCE_PATH}/xfce/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
